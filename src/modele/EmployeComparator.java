82876b58e9128a479cdb5ef2e22ddd0f9446da65
Francis Lafontaine <lafontainefra@gmail.com>
2022-06-08 15:24:43 -0400

    commit créations registre Employe

---
+++ src/modele/EmployeComparator.java
@@ -0,0 +1,11 @@
+package modele;
+
+import java.util.Comparator;
+
+public class EmployeComparator implements Comparator<Employe> {
+
+    @Override
+    public int compare(Employe o1, Employe o2) {
+        return (o1.getNom().compareTo(o2.getNom() ));
+    }
+}
--- src/modele/RegistreEmploye.java
+++ src/modele/RegistreEmploye.java
@@ -1,38 +1,56 @@
 package modele;
 
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.TreeSet;
+
 public class RegistreEmploye {
+    private TreeSet<Employe> registre;
 
+    public RegistreEmploye() {
+        registre = new TreeSet<>(new EmployeComparator());//------------------fair un choix comparator ou comparable
+    }
 
+    public TreeSet<Employe> getRegistre() {
+        return registre;
+    }
 
+    public void setRegistre(TreeSet<Employe> registre) {
+        this.registre = registre;
+    }
 
+    public void ajouterEmploye(Employe employe) {
+        registre.add(employe);
+        JOptionPane.showMessageDialog(null, "Ajout d'un employé avec succès",
+                "Edition", JOptionPane.INFORMATION_MESSAGE);
+    }
 
+    public String afficherEmploye() {
+        StringBuilder sb = new StringBuilder();
+        for (Employe tmp : registre
+        ) {
+            sb.append(tmp);
+            sb.append("\n");
+        }
+        return sb.toString();
+    }
 
+    public ArrayList<Employe> rechercherEmploye(String motCle) {
+        ArrayList<Employe> trouve = new ArrayList<>();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//    public void ajouterCommerciaux() {
-//    }
+        for (Employe tmp : registre
+        ) {
+            if (tmp.getNom().equals(motCle)) {
+                trouve.add(tmp);
+            }
+        }
+        return trouve;
+    }
 //
 //    public void supprimerCommerciaux() {
 //    }
 //
-//    public void afficherCommerciaux() {
-//    }
+
 //
 //    public void validerDoublonCommerciaux() {
 //    }
