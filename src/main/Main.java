cca91dddbddecfb8bd411c7f65606fd4e56fc30c
MaximePoulin899 <maxime.poulin.artiste@gmail.com>
2022-06-10 12:18:12 -0400

    listerFrais

--- src/main/Main.java
+++ src/main/Main.java
@@ -71,64 +71,70 @@ public class Main {
 
         listing.listerEmployes();
 
-//
-//        //----------------Test des RemboDispo Hebergement/Restaurant/Transport-------------------
-//        Date date = new Date();
-//        Employe empJunior = new Employe("Frank", "Boy", "junior",0,0,0);
-//        Employe empSenior = new Employe("Max", "Boy", "senior",0,0,0);
-//        Employe empSuper= new Employe("King", "Pin", "super",0,0,0);
-//        Hebergement hebergement1 = new Hebergement(empJunior,"Hebergement",100,date);
-//        Hebergement hebergement2 = new Hebergement(empSenior,"Hebergement",100,date);
-//        Hebergement hebergement3 = new Hebergement(empSuper,"Hebergement",100,date);
-//
-//        // Creation de frais et test de rembo dispo
-//        hebergement1.setRemboDispo(Utilitaire.calculRemboursementMaxHebergement(empJunior));
-//        hebergement2.setRemboDispo(Utilitaire.calculRemboursementMaxHebergement(empSenior));
-//        hebergement3.setRemboDispo(Utilitaire.calculRemboursementMaxHebergement(empSuper));
-//        Transport transport = new Transport(empJunior,"Transport",100,date);
-//        transport.setRemboDispo(Utilitaire.calculRemboursementMaxTransport(empJunior));
-//        Restauration restauration = new Restauration(empSenior,"Restauration",100,date);
-//        restauration.setRemboDispo(Utilitaire.calculRemboursementMaxRestaurant(empSenior));
-//
-////
-////        System.out.println(hebergement1);
-////        System.out.println(hebergement2);
-////        System.out.println(hebergement3);
-////        System.out.println(transport);
-////        System.out.println(restauration);
-//
-//
-//        //Creer listing Frais
-//        RegistreFrais listingFrais = new RegistreFrais();
-//
-//        // ajouter facture
-////        try {
-////            listingFrais.ajouterFrais(hebergement1);
-////        } catch (ExceptionFraisExisteDeja e) {
-////            throw new RuntimeException(e);
-////        }
-////        try {
-////            listingFrais.ajouterFrais(hebergement2);
-////        } catch (ExceptionFraisExisteDeja e) {
-////            throw new RuntimeException(e);
-////        }
-//////        listingFrais.ajouterFrais(transport);
-//////       listingFrais.ajouterFrais(restauration);
-//
-//        //ajouter frais 2
-//        listingFrais.ajouterFrais2(hebergement1);
-//        listingFrais.ajouterFrais2(hebergement2);
-//        listingFrais.ajouterFrais2(transport);
-//        listingFrais.ajouterFrais2(restauration);
-//
-//
-//       //Afficher Frais
-//        System.out.println("-------------------------------------");
-//        listingFrais.listerFrais();
-//
-//
-//
-//        //----------------Test des RemboDispo Hebergement/Restaurant/Transport-------------------
+
+        //----------------Test des RemboDispo Hebergement/Restaurant/Transport-------------------
+        Date date = new Date();
+        Employe empJunior = new Employe("Frank", "Boy", "junior",0,0,0);
+        Employe empSenior = new Employe("Max", "Boy", "senior",0,0,0);
+        Employe empSuper= new Employe("King", "Pin", "super",0,0,0);
+        Hebergement hebergement1 = new Hebergement(empJunior,"Hebergement",100,date);
+        Hebergement hebergement2 = new Hebergement(empSenior,"Hebergement",100,date);
+        Hebergement hebergement3 = new Hebergement(empSuper,"Hebergement",100,date);
+
+        // Creation de frais et test de rembo dispo
+        hebergement1.setRemboDispo(Utilitaire.calculRemboursementMaxHebergement(empJunior));
+        hebergement2.setRemboDispo(Utilitaire.calculRemboursementMaxHebergement(empSenior));
+        hebergement3.setRemboDispo(Utilitaire.calculRemboursementMaxHebergement(empSuper));
+        Transport transport = new Transport(empJunior,"Transport",100,date);
+        transport.setRemboDispo(Utilitaire.calculRemboursementMaxTransport(empJunior));
+        Restauration restauration = new Restauration(empSenior,"Restauration",100,date);
+        restauration.setRemboDispo(Utilitaire.calculRemboursementMaxRestaurant(empSenior));
+
+
+        System.out.println(hebergement1);
+        System.out.println(hebergement2);
+        System.out.println(hebergement3);
+        System.out.println(transport);
+        System.out.println(restauration);
+
+
+        //Creer listing Frais
+        RegistreFrais listingFrais = new RegistreFrais();
+
+//       //  ajouter facture
+        try {
+            listingFrais.ajouterFrais(transport);
+        } catch (ExceptionFraisExisteDeja e) {
+            JOptionPane.showMessageDialog(null,"Erreur! Frais en double\n","Erreur Ajout Frais",JOptionPane.ERROR_MESSAGE);
+        }
+
+        try {
+            listingFrais.ajouterFrais(transport);
+        } catch (ExceptionFraisExisteDeja e) {
+            JOptionPane.showMessageDialog(null,"Erreur! Frais en double\n","Erreur Ajout Frais",JOptionPane.ERROR_MESSAGE);
+        }
+
+
+        try {
+            listingFrais.ajouterFrais(hebergement2);
+        } catch (ExceptionFraisExisteDeja e) {
+            JOptionPane.showMessageDialog(null,"Erreur! Frais en double\n","Erreur Ajout Frais",JOptionPane.ERROR_MESSAGE);
+        }
+
+        try {
+            listingFrais.ajouterFrais(restauration);
+        } catch (ExceptionFraisExisteDeja e) {
+            JOptionPane.showMessageDialog(null,"Erreur! Frais en double\n","Erreur Ajout Frais",JOptionPane.ERROR_MESSAGE);
+        }
+
+
+       //Afficher Frais
+        System.out.println("\n--------------Afficher Frais-------------------\n");
+        listingFrais.listerFrais();
+
+
+
+        //----------------Test des RemboDispo Hebergement/Restaurant/Transport-------------------
 
 
 
--- src/modele/RegistreFrais.java
+++ src/modele/RegistreFrais.java
@@ -2,66 +2,56 @@ package modele;
 
 
 import utils.ExceptionFraisExisteDeja;
-
 import javax.swing.*;
-import java.time.chrono.HijrahEra;
-import java.util.Iterator;
 import java.util.TreeSet;
 
 public class RegistreFrais {
-    public TreeSet registreFrais;
+    public TreeSet<Frais> registreFrais;
 
     public RegistreFrais() {
         this.registreFrais = new TreeSet<>(new FraisComparator());
     }
 
-    public TreeSet getRegistreFrais() {
+    public TreeSet<Frais> getRegistreFrais() {
         return registreFrais;
     }
 
-    public void setRegistreFrais(TreeSet registreFrais) {
+    public void setRegistreFrais(TreeSet<Frais> registreFrais) {
         this.registreFrais = registreFrais;
     }
 
-//    public void ajouterFrais(Frais frais) throws ExceptionFraisExisteDeja {
-//        if (validerDoublonFrais(frais)){
-//            System.out.println("Erreur");
-//            throw new ExceptionFraisExisteDeja("Un frais doublon trouvé", frais);
-//        }else {
-//            JOptionPane.showMessageDialog(null,"Ajout du frais de votre employé avec succès ","Edition", JOptionPane.INFORMATION_MESSAGE);
-//            System.out.println("good Frais");
-//            this.registreFrais.add(frais);
-//        }
-//    }
-
-    public void ajouterFrais2(Frais frais){
-        this.registreFrais.add(frais);
+    public void ajouterFrais(Frais frais) throws ExceptionFraisExisteDeja {
+        if (validerDoublonFrais(frais)) {
+            System.out.println("Erreur");
+            throw new ExceptionFraisExisteDeja("Un frais doublon trouvé", frais);
+        } else {
+            JOptionPane.showMessageDialog(null, "Ajout du frais de votre employé avec succès ", "Edition", JOptionPane.INFORMATION_MESSAGE);
+            System.out.println("good Frais");
+            this.registreFrais.add(frais);
+        }
     }
+
+
 
     public void supprimerFrais(Frais frais) {
         this.registreFrais.remove(frais);
     }
 
     public void listerFrais() {
-        Iterator iterateur = registreFrais.iterator();
-        while (iterateur.hasNext()){
-            System.out.println(iterateur.next());
+        for (Frais tmp : registreFrais) {
+            System.out.println(tmp);
         }
     }
 
 
-//
+    //
 //    public String afficherFrais() {
-//        StringBuilder sb = new StringBuilder();
-//        for (Frais tmp : registreFrais  ) {
-//
 //        }
 //    }
 //
     public boolean validerDoublonFrais(Frais frais) {
-        Iterator iterateur = registreFrais.iterator();
-        while (iterateur.hasNext()){
-            if(iterateur.equals(frais)){
+        for (Frais tmp : registreFrais) {
+            if (tmp.equals(frais)) {
                 return true;
             }
         }
